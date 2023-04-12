3a874c4930c3fe1fa38b1303ec803cf79ae7cabf
Julio Garcia <jcgarciam0892@gmail.com>
2023-04-12 19:50:09 -0400

    updating angular version

--- src/app/layout/information-layout/information-layout.component.spec.ts
+++ src/app/layout/information-layout/information-layout.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { InformationLayoutComponent } from './information-layout.component';
 
@@ -6,7 +6,7 @@ describe('InformationLayoutComponent', () => {
   let component: InformationLayoutComponent;
   let fixture: ComponentFixture<InformationLayoutComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ InformationLayoutComponent ]
     })
